06fc032b890b21f7628c0c462141beb40b7d4a82
vaseksindelaru <vaseksindelaru@gmail.com>
2023-12-03 21:07:29 +0100

    oooo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const app = express();
 app.get('/', (req, res) => {
   console.log('peticion recibida');
-  res.status(200).send('<h1>peticion recibida holanm mundo</h1>');
+  res.status(200).send('<h1>peticion recibida holanoooooom mundo</h1>');
   
 });
 const PORT = process.env.PORT || 3000 ;
