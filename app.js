ea9b3220666d91fb080b54787b38a0c9451e1e03
vaseksindelaru <vaseksindelaru@gmail.com>
2023-12-03 15:12:04 +0100

    fffafa

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>peticion recibida holannmmmmm mundo</h1>');
   
 });
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000 ;
 app.listen(PORT, () => {
   console.log(`Escuchando en el puerto ${PORT}`);
 });
