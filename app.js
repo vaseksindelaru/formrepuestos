51ec3aca8584b82d9c236440970cf6cdee37ef84
vaseksindelaru <vaseksindelaru@gmail.com>
2023-12-03 13:08:27 +0100

    port

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('<h1>peticion recibida con nodemon<h1>');
 });
-
-app.listen(3000, () => {
-  console.log('Escuchando en el puerto 3000');
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Escuchando en el puerto ${PORT}`);
 });
